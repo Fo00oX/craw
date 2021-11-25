cbd7062140b8a4473f6703beed54e0658c0ce599
Stefan <stefan.schindler@stud.fh-campuswien.ac.at>
2021-11-25 18:16:02 +0100

    Added simple Test classes testing if the Build works

---
+++ craw-cli/src/test/java/at/ac/fhcampuswien/craw/cli/CliModuleTest.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.craw.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CliModuleTest {
+    @Test
+    void testBuild() {
+        assertTrue(true);
+    }
+}
---
+++ craw-lib/src/test/java/at/ac/fhcampuswien/craw/lib/LibModuleTest.java
@@ -0,0 +1,11 @@
+package at.ac.fhcampuswien.craw.lib;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LibModuleTest {
+    @Test
+    void testBuild(){
+        assertTrue(true);
+    }
+}
