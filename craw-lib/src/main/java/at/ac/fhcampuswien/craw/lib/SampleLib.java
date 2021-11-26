61b68a6ba506bbd5158ef137b9cbe4335e8681f1
Stefan <stefan.schindler@stud.fh-campuswien.ac.at>
2021-11-26 18:23:56 +0100

    Added TODO

--- craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/SampleLib.java
+++ craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/SampleLib.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien.craw.lib;
 
+//TODO sample for Test, remove as soon as other code is added
 public class SampleLib {
     public String getSampleMessage(){
         return "Test Library connection";
