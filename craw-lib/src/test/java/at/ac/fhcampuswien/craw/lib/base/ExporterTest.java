0b0a779634f366c97bdcf15f15b3aa5dffabe876
christophthonhauser <c.thonhauser97@gmail.com>
2021-12-19 16:19:22 +0100

    init method tests

---
+++ craw-lib/src/test/java/at/ac/fhcampuswien/craw/lib/base/ExporterTest.java
@@ -0,0 +1,18 @@
+package at.ac.fhcampuswien.craw.lib.base;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExporterTest {
+
+    @Test
+    void writeYAML() {
+        assertTrue(true);
+    }
+
+    @Test
+    void writeJSON() {
+        assertTrue(true);
+    }
+}
