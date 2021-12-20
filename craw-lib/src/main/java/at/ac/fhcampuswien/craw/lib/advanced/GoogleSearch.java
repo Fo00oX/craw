f68d8f671bcf24519f5facf5a93522720e1fa004
Stefan <stefan.schindler@stud.fh-campuswien.ac.at>
2021-12-20 19:43:56 +0100

    Added sample data to Google Search

--- craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/advanced/GoogleSearch.java
+++ craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/advanced/GoogleSearch.java
@@ -2,12 +2,20 @@ package at.ac.fhcampuswien.craw.lib.advanced;
 
 import at.ac.fhcampuswien.craw.lib.model.Weblink;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GoogleSearch {
-    public List<Weblink> searchQuery(String query, int anz){
+    public List<Weblink> searchQuery(String query, int anz) {
         //TODO add functionality
-        return new ArrayList<>();
+        return List.of(
+                Weblink.builder()
+                        .URL("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiO7e6og_P0AhWBh_0HHcY5DmcQFnoECA0QAQ&url=https%3A%2F%2Fwww.fh-campuswien.ac.at%2F&usg=AOvVaw3xp-GzQKJbEHJ3wxY7gCZ5")
+                        .Name("FH Campus Wien")
+                        .build(),
+                Weblink.builder()
+                        .URL("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiO7e6og_P0AhWBh_0HHcY5DmcQFnoECAUQAQ&url=https%3A%2F%2Fde.wikipedia.org%2Fwiki%2FFH_Campus_Wien&usg=AOvVaw2pwpFR0XhPSm99QMWmWTaF")
+                        .Name("FH Campus Wien - Wikipedia")
+                        .build()
+        );
     }
 }
