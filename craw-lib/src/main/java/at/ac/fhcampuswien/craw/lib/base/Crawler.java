549acc2fee505c967c4be5be7bf64f8ea97a7b44
Stefan <stefan.schindler@stud.fh-campuswien.ac.at>
2021-12-20 18:13:06 +0100

    Added Testdata to Crawler method

--- craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/base/Crawler.java
+++ craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/base/Crawler.java
@@ -2,15 +2,24 @@ package at.ac.fhcampuswien.craw.lib.base;
 
 import at.ac.fhcampuswien.craw.lib.model.Weblink;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Provides functionality to download and parse Websites
  */
 public class Crawler {
-    public List<Weblink> getLinks(String link){
+    public List<Weblink> getLinks(String link) {
         //TODO add functionality
-        return new ArrayList<>();
+        return List.of(
+                // Testdata
+                Weblink.builder()
+                        .Name("Link1")
+                        .URL("https://track.craw.dataspeak.at/")
+                        .build(),
+                Weblink.builder()
+                        .Name("Link2")
+                        .URL("https://track.craw.dataspeak.at/projects/61edb02c-bc65-4a0a-a71b-9b0639200aba/")
+                        .build()
+        );
     }
 }
--- craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/model/Weblink.java
+++ craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/model/Weblink.java
@@ -1,11 +1,11 @@
 package at.ac.fhcampuswien.craw.lib.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Weblink {
     private String URL;
     private String Name;
-
-    // TODO Getter + Setters
 }
