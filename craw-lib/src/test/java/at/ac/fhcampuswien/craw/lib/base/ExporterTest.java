1b695042ee8cd78467c1ee384bf73f3655237927
christophthonhauser <c.thonhauser97@gmail.com>
2021-12-21 18:28:45 +0100

    test json file export

--- craw-lib/src/test/java/at/ac/fhcampuswien/craw/lib/base/ExporterTest.java
+++ craw-lib/src/test/java/at/ac/fhcampuswien/craw/lib/base/ExporterTest.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien.craw.lib.base;
 
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -11,8 +12,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExporterTest {
 
-    Path path1, path2;
-    File file1, file2;
+    Path path;
+    File file;
 
     /* This directory and the files created in it will be deleted after
      * tests are run, even in the event of failures or exceptions.
@@ -24,50 +25,37 @@ public class ExporterTest {
     @BeforeEach
     public void setUp() {
         try {
-            path1 = tempDir.resolve( "testfile1.json" );
-            path2 = tempDir.resolve( "testfile2.jml" );
-        }
-        catch( InvalidPathException ipe ) {
-            System.err.println(
-                    "error creating temporary test file in " +
-                            this.getClass().getSimpleName() );
+            path = tempDir.resolve("testfile1.json");
+        } catch (InvalidPathException ipe) {
+            System.err.println("error creating temporary test file in " + this.getClass().getSimpleName());
         }
 
-        file1 = path1.toFile();
-        file2 = path2.toFile();
+        file = path.toFile();
     }
 
     /**
-     *  Write to the two temporary files and run some asserts.
+     * Write to the two temporary files and run some asserts.
      */
     @Test
-    public void test2TempFiles() {
+    public void writeJsonToFile() {
 
         //write out data to the test files
         try {
-            FileWriter fw1 = new FileWriter( file1 );
-            BufferedWriter bw1 = new BufferedWriter( fw1 );
-            bw1.write( "content for file1");
+            FileWriter fw1 = new FileWriter(file);
+            BufferedWriter bw1 = new BufferedWriter(fw1);
+
+            JSONObject testJSON = new JSONObject();
+            testJSON.put("Link1", "www.google.at");
+
+            bw1.write(testJSON.toJSONString());
             bw1.close();
-
-            FileWriter fw2 = new FileWriter( file2 );
-            BufferedWriter bw2 = new BufferedWriter( fw2 );
-            bw2.write( "content for file2" );
-            bw2.close();
-        }
-        catch( IOException ioe ) {
-            System.err.println(
-                    "error creating temporary test file in " +
-                            this.getClass().getSimpleName() );
+        } catch (IOException ioe) {
+            System.err.println("error creating temporary test file in " + this.getClass().getSimpleName());
         }
 
-        assertTrue( file1.exists() );
-        assertTrue( file2.isFile() );
+        assertTrue(file.exists());
+        assertTrue(file.isFile());
 
-        assertEquals( file1.length(), 17L );
-        assertEquals( file1.length(), file2.length() );
-
-        assertTrue( file1.getAbsolutePath().endsWith(
-                "testfile1.json" ));
+        assertTrue(file.getAbsolutePath().endsWith("testfile1.json"));
     }
 }
