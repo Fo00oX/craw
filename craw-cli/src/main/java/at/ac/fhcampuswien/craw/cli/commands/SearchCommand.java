bc672dfbd10e3ccfe08e6f82b063418ebd5e6cf5
Stefan <stefan.schindler@stud.fh-campuswien.ac.at>
2021-12-25 16:10:43 +0100

    Added Basic SearchCommand and Test to ensure that the required query is correctly queried

--- craw-cli/src/main/java/at/ac/fhcampuswien/craw/cli/commands/SearchCommand.java
+++ craw-cli/src/main/java/at/ac/fhcampuswien/craw/cli/commands/SearchCommand.java
@@ -1,20 +1,46 @@
 package at.ac.fhcampuswien.craw.cli.commands;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Model.CommandSpec;
 
-import java.util.concurrent.Callable;
+import java.util.List;
 
-@CommandLine.Command(
+import static picocli.CommandLine.*;
+
+@Command(
         name = "search",
-        description = "Run a Google search using a specified request"
+        description = "Run a Google search using a specified query."
 )
 public class SearchCommand extends BaseCommand {
     @Spec
     CommandSpec spec;
 
+    @Parameters(
+            paramLabel = "query",
+            index = "0..*",
+            arity = "1..*",
+            description = ""
+    )
+    List<String> queryStrings;
+
+    @Option(
+            names = {"-r", "-n", "--results"},
+            paramLabel = "nrOfResults",
+            description = "The number of results to fetch. Defaults to ${DEFAULT-VALUE}.",
+            defaultValue = "10"
+    )
+    int nrResults;
+
+
+    /**
+     * @return the full query joined into a single String
+     */
+    private String getQuery() {
+        return String.join("", queryStrings);
+    }
+
     @Override
     public void run() {
-        spec.commandLine().getOut().println("SearchCommand!");
+        if (getQuery().isBlank()) // Empty values bypass the picocli validation that otherwise ensures that a query is specified, so they have to be filtered here
+            throw new ParameterException(spec.commandLine(), "The required parameter query was not specified.");
     }
 }
--- craw-cli/src/test/java/at/ac/fhcampuswien/craw/cli/SearchCommandTests.java
+++ craw-cli/src/test/java/at/ac/fhcampuswien/craw/cli/SearchCommandTests.java
@@ -1,8 +1,10 @@
 package at.ac.fhcampuswien.craw.cli;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchCommandTests extends CliTestBase {
@@ -19,6 +21,24 @@ public class SearchCommandTests extends CliTestBase {
 
         // assert
         expectSuccessExitCode();
+        assertTrue(output.contains("Usage: craw search"));
+        assertTrue(output.contains("--help"));
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = {"  ", "\n", "\r"})
+    void testFailOnNoArguments(String arg) {
+        // act
+        exitCode = cmd.execute("search", arg);
+        String output = err.toString();
+
+        // assert
+        expectFailedExitCode();
+        assertEquals(
+                "The required parameter query was not specified.",
+                output.substring(0, output.indexOf('\r'))
+        ); // Verify correct error message is printed. After the error message Usage help is automatically printed after a newline.
         assertTrue(output.contains("Usage: craw search"));
         assertTrue(output.contains("--help"));
     }
