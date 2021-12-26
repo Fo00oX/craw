5f9e4f419a01fcbf82c9744d8a43dbcf7643e293
Denis Radovanovic <denis.radovanovic@stud.fh-campuswien.ac.at>
2021-12-26 23:55:58 +0100

    Implement first functionalities of start-up script to Powershell

--- start.ps1
+++ start.ps1
@@ -1,17 +1,72 @@
-#check if container exists
-if (docker ps -a | Select-String -Pattern "craw-cli") {
+function build {
+  echo "Building craw-cli image ..."
+  docker build -t craw-cli .
+}
+
+function run {
+  echo "Running craw-cli container..."
+  docker run -d --name craw-cli craw-cli
+}
+
+function start {
+  echo "Starting craw-cli container..."
+  docker start craw-cli
+}
+
+function rebuild {
+  echo "Removing craw-cli container..."
   docker stop craw-cli
   docker rm craw-cli
-}
-
-#check if image exists
-if (docker images | Select-String -Pattern "craw-cli") {
+  echo "Removing craw-cli image..."
   docker rmi craw-cli
+  build
+  run
 }
 
-#build the image
-docker build -t craw-cli .
+if ( $args.count -eq 0 ) {
+  if (docker images | Select-String -Pattern "craw-cli") {
+    if (!(docker ps -f status=running | Select-String -Pattern "craw-cli")) {
+      if (!(docker ps -f status=exited | Select-String -Pattern "craw-cli")) {
+        run
+      }
+      else {
+        start
+      }
+    }
+    else {
+      rebuild
+    }
+  }
+  else{
+    build
+    run
+  }
+}
 
-#start the container
-docker run -d --name craw-cli craw-cli
+if ( $args[0] -eq "--rebuild") {
+  rebuild
+}
+
+if ( $args[0] -eq "--build" ) {
+  build
+}
+
+if ( $args[0] -eq "--run" ) {
+  run
+}
+
+if ( $args[0] -eq "--start" ) {
+  start
+}
+
+if ( $args[0] -eq "--help" ) {
+  echo "Usage: ./start.sh <arg>
+
+args (optional):
+--rebuild   removes the image and redeploys a new container with a new image
+--build     builds the craw-cli image
+--run       launches a non-existent craw-cli container from a pre-built image
+--start     starts an exited (existing) craw-cli container
+--help      prints a help message"
+}
 
