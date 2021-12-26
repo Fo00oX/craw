36c3b369c566596f61d810fb2e11299b0922b76c
Denis Radovanovic <denis@stellar-room.at>
2021-12-26 23:21:52 +0100

    Enhance functionality of start-up script

--- start.sh
+++ start.sh
@@ -1,21 +1,78 @@
 #!/bin/bash
+function build () {
+  echo "Building craw-cli image ..."
+  docker build -t craw-cli .
+}
 
-#check if container exists
-if docker ps -a | grep -q "craw-cli"; 
-then
+function run () {
+  echo "Running craw-cli container..."
+  docker run -d --name craw-cli craw-cli
+}
+
+function start() {
+  echo "Starting craw-cli container..."
+  docker start craw-cli
+}
+
+function rebuild () {
+  echo "Removing craw-cli container..."
   docker stop craw-cli
   docker rm craw-cli
-fi
+  echo "Removing craw-cli image..."
+  docker rmi craw-cli 
+  build
+  run
+}
 
-#check if image exists
-if docker images | grep -q "craw-cli"; 
+if [ $# -eq 0 ]
 then
-  docker rmi craw-cli
+  if (docker images | grep "craw-cli");
+  then
+    if ! (docker ps -f status=running | grep -q "craw-cli")
+    then
+      if ! (docker ps -f status=exited | grep -q "craw-cli");
+      then
+        run
+      else
+        start
+      fi
+    else
+      rebuild
+    fi
+  else
+    build
+    run
+  fi
 fi
 
-#build the image
-docker build -t craw-cli .
+if [ "$1" = "--rebuild" ]
+then
+  rebuild
+fi
 
-#start the container
-docker run -d --name craw-cli craw-cli
+if [ "$1" = "--build" ]
+then
+  build
+fi
 
+if [ "$1" = "--run" ]
+then
+  run
+fi
+
+if [ "$1" = "--start" ]
+then
+  start
+fi
+
+if [ "$1" = "--help" ]
+then
+  echo "Usage: ./start.sh <arg>
+
+args (optional):
+--rebuild   removes the image and redeploys a new container with a new image
+--build     builds the craw-cli image
+--run       launches a non-existent craw-cli container from a pre-built image 
+--start     starts an exited (existing) craw-cli container 
+--help      prints a help message"
+fi
