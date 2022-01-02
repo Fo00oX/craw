89ac6e12f589b8afab4509d7e2c762d20ba1580f
tacsi566 <zsolt.zombori11@gmail.com>
2022-01-02 22:38:10 +0100

    Cleaned up import in BrokenLink Made linkStatus final and not static. Added description to enum States.

--- craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/model/BrokenLink.java
+++ craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/model/BrokenLink.java
@@ -1,13 +1,16 @@
 package at.ac.fhcampuswien.craw.lib.model;
 
-import at.ac.fhcampuswien.craw.lib.model.Weblink;
-
 public class BrokenLink extends Weblink {
 
-    // IntelliJ suggested to make this private. Rethink if it makes sense.
-    private static States linkStatus;
+    // IntelliJ suggested to make this final. Rethink if it makes sense. It also makes sense not to be static.
+    private final States linkStatus;
 
-    // For filtering purposes later on.
+    /*
+        For filtering purposes later on.
+        MALFORMED is when the link is not valid and could not be parsed
+        CONNECTION_ERROR is when there was an error writing/reading from or connecting to the socket
+        NOT_OK is when the HTTP response code != 200
+    */
     public enum States {
         MALFORMED,
         CONNECTION_ERROR,
