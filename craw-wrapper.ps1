e161dd4d220a7de8e3ce87f62dead7f5c2b0da72
Denis Radovanovic <denis@stellar-room.at>
2022-01-05 19:31:14 +0100

    Add option for craw-wrapper to start the container on Windows

--- craw-wrapper.ps1
+++ craw-wrapper.ps1
@@ -1,2 +1,13 @@
-docker exec -t craw-cli craw-cli $args 
+if (!(docker ps -f status=running | Select-String -Pattern "craw-cli")) {
+  echo "It seems there is no craw container running :("
+  $input = Read-Host "Do you want to start it? [y/n] "
+  while("y", "n" -notcontains $input) {
+    $input = Read-Host "Do you want to start it? [y/n] "
+  }
+  switch ($input) {
+    "y" { ./start.ps1 }
+    "n" { exit 1 }
+  }
+}
 
+docker exec -t craw-cli craw-cli $args 
