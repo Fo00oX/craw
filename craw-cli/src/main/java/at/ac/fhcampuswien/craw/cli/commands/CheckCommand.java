2dea06b050460adf914a19a1f523dbc4f47b966f
Stefan <stefan.schindler@stud.fh-campuswien.ac.at>
2022-01-07 15:46:48 +0100

    Fixed accidentally leftover query and -r Option/Parameter from CheckCommand

--- craw-cli/src/main/java/at/ac/fhcampuswien/craw/cli/commands/CheckCommand.java
+++ craw-cli/src/main/java/at/ac/fhcampuswien/craw/cli/commands/CheckCommand.java
@@ -9,36 +9,13 @@ import at.ac.fhcampuswien.craw.lib.model.Weblink;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static picocli.CommandLine.*;
+import static picocli.CommandLine.Command;
 
 @Command(
         name = "check",
         description = "Check a page for broken links."
 )
 public class CheckCommand extends BaseLinkOutputCommand {
-
-    @Parameters(
-            paramLabel = "query",
-            index = "0..*",
-            arity = "1..*",
-            description = ""
-    )
-    private List<String> queryStrings;
-
-    @Option(
-            names = {"-r", "-n", "--results"},
-            paramLabel = "nrOfResults",
-            description = "The number of results to fetch. Defaults to ${DEFAULT-VALUE}.",
-            defaultValue = "10"
-    )
-    private int nrResults;
-
-    /**
-     * @return the full query joined into a single String
-     */
-    private String getQuery() {
-        return String.join(" ", queryStrings);
-    }
 
     @Override
     public void run() {
