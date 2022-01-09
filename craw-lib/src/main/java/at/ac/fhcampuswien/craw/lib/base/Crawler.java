ccfd97e3a15e0c59500c5cf911ea36427828a297
WildingGerald <gerald.wilding@stud.fh-campuswien.ac.at>
2022-01-09 06:36:10 +0100

    adapted to Pull request changes. Parameterized Testing. Added Error handling.

--- craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/base/Crawler.java
+++ craw-lib/src/main/java/at/ac/fhcampuswien/craw/lib/base/Crawler.java
@@ -1,11 +1,14 @@
 package at.ac.fhcampuswien.craw.lib.base;
 
+import at.ac.fhcampuswien.craw.lib.exceptions.CrawException;
 import at.ac.fhcampuswien.craw.lib.model.Weblink;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,36 +20,64 @@ public class Crawler {
 
     int depth;
 
-    Crawler(int depth){
+    public Crawler(int depth){
         this.depth = depth;
     }
-    Crawler(){
+    public Crawler(){
         this.depth = 1;
     }
 
-    public ArrayList<Weblink> getLinks(String link){
+    /**
+     * Searches a website for links
+     * @param url of Website to crawl
+     * @return Arraylist of Weblinks
+     * @throws CrawException Is thrown when teh URL is malformed or the website responds with an HTTP error
+     * @throws Exception Is thrown for unexpected errors
+     */
+    public ArrayList<Weblink> getLinks(String url) throws CrawException, Exception {
         ArrayList<Weblink> list = new ArrayList<>();
-        Pattern pattern = Pattern.compile("<a.+href=\"(.*?)\"[^>]*>(.*?)</a>");
-        Matcher matcher = pattern.matcher(getHtml(link));
-        while(matcher.find()){
-            list.add(new Weblink(matcher.group(1), matcher.group(2)));
+        try {
+            //the regex looks for the pattern <a href="x"> "y"</a>
+            //afterwards groups (x, y) are used to access collected information
+            //for throughout explanation of the regex look at external sources like https://regex101.com/
+            Pattern pattern = Pattern.compile("<a.+href=\"(.*?)\"[^>]*>(.*?)</a>");
+            Matcher matcher = pattern.matcher(getHtml(url));
+            while (matcher.find()) {
+                list.add(new Weblink(matcher.group(1), matcher.group(2)));
+            }
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new CrawException("Url wasn't properly formed. " + e.getMessage(), e);
+        } catch (IOException e){
+            if (e.getMessage().contains("HTTP")) {
+                throw new CrawException(e.getMessage(), e);
+            } else {
+                throw new Exception(e);
+            }
+        }catch(Exception e){
+
+            throw new Exception(e);
         }
         return list;
     }
 
-    public String getHtml(String link){
-        String html = null;
+    /**
+     * Returns HTML of a requested website
+     * @param url of Website to get
+     * @return html as String
+     * @throws Exception is thrown by errors in Libaries
+     */
+    private String getHtml(String url) throws Exception {
+        String html;
         URLConnection connection;
-        try {
-            connection = new URL(link).openConnection();
-            Scanner scanner = new Scanner(connection.getInputStream());
-            //delimiter ensures that we get the entire HTML
-            scanner.useDelimiter("\\Z");
-            html = scanner.next();
-            scanner.close();
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+        URL testedUrl = new URL(url);
+        //testing the url for invalid characters
+        testedUrl.toURI();
+        connection = testedUrl.openConnection();
+        Scanner scanner = new Scanner(connection.getInputStream());
+        //delimiter ensures that we get the entire HTML
+        scanner.useDelimiter("\\Z");
+        html = scanner.next();
+        scanner.close();
         return html;
     }
 }
